f169faf502b137540c036295468ca7e57a2f3958
andre2610 <dre2610@gmail.com>
2022-03-27 21:16:16 +0200

    completed simple random user generator tutorial in vue using cdn script

--- app.js
+++ app.js
@@ -8,6 +8,20 @@ const app = Vue.createApp({
       picture: "https://randomuser.me/api/portraits/men/10.jpg",
     };
   },
+  methods: {
+    async getUser() {
+      const { results } = await fetch("https:/randomuser.me/api").then((res) =>
+        res.json()
+      );
+      const { name, email, gender, picture } = results[0];
+
+      this.firstName = name.first;
+      this.lastName = name.last;
+      this.email = email;
+      this.gender = gender;
+      this.picture = picture.large;
+    },
+  },
 });
 
 app.mount("#app");
